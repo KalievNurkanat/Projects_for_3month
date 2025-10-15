b670faed64373801aa343f8f5744a9458b852e9c
KalievNurkanat <kalievnurkanat89@gmail.com>
2025-10-15 12:34:00 +0600

    hw4

--- main.py
+++ main.py
@@ -16,20 +16,11 @@ def main(page: ft.Page):
     def on_button_click(_):
         timestamp = datetime.now().strftime("%H:%M:%S")
         if name_input.value and len(name_input.value) <= 12:
-            greeting_history.append((timestamp, name_input.value))
-            spans_submit = []
-            if page.theme_mode == ft.ThemeMode.DARK:
-                time_color = ft.Colors.WHITE
-                name_color = ft.Colors.BLUE
-            else:
-                time_color = ft.Colors.BLACK
-                name_color = ft.Colors.RED
+            greeting_history.append(ft.TextSpan(f"{timestamp}| {name_input.value}\n",
+                                                style=ft.TextStyle(color=ft.Colors.BLUE,
+                                                weight=ft.FontWeight.BOLD)))
 
-            for ts, val in greeting_history:
-                spans_submit.append(ft.TextSpan(f"{ts} ", style=ft.TextStyle(color=time_color)))
-                spans_submit.append(ft.TextSpan(f"{val}\n", style=ft.TextStyle(color=name_color, weight=ft.FontWeight.BOLD)))
-
-            history_text.spans = spans_submit
+            history_text.spans = greeting_history
 
             if now.hour >= 6 and now.hour < 12:
                 greeting_text.value = f"Good morning {name_input.value}"
@@ -56,41 +47,37 @@ def main(page: ft.Page):
         page.update()
             
     def button_change_theme(_):
-        spans_click=[]
         if page.theme_mode == ft.ThemeMode.DARK:
            page.theme_mode = ft.ThemeMode.LIGHT
-           for ts, val in greeting_history:
-               spans_click.append(ft.TextSpan(f"{ts} ", style=ft.TextStyle(color=ft.Colors.BLACK)))
-               spans_click.append(ft.TextSpan(f"{val}\n",
-                                               style=ft.TextStyle(color=ft.Colors.RED,
-                                               weight=ft.FontWeight.BOLD)))
-               
+          
         else:
             page.theme_mode = ft.ThemeMode.DARK
-            for ts, val in greeting_history:
-                spans_click.append(ft.TextSpan(f"{ts} ", style=ft.TextStyle(color=ft.Colors.WHITE)))
-                spans_click.append(ft.TextSpan(f"{val}\n",
-                                                style=ft.TextStyle(color=ft.Colors.BLUE,
-                                                weight=ft.FontWeight.BOLD)))
-
-        history_text.spans = spans_click
-        
+            
         nonlocal counter
         counter += 1
         counter_theme_text.value = f"Theme was changed {counter} times"
         page.update()
 
+    def clear_button(_):
+        greeting_history.clear()
+        history_text.value = "Story of greetings: \n"
+        page.update()
+
     name_input = ft.TextField(label="Enter your name: ",on_submit=on_button_click)
     button_for_theme = ft.ElevatedButton("CHANGE THEME",ft.Icons.BRIGHTNESS_7,height=85,width=105,on_click=button_change_theme)
+    button_clear = ft.IconButton(icon=ft.Icons.DELETE,on_click=clear_button,height=85,width=105)
 
     theme_button = ft.Row([button_for_theme], alignment = ft.MainAxisAlignment.CENTER)
     theme_text = ft.Row([counter_theme_text], alignment = ft.MainAxisAlignment.CENTER)
     story_greeting_text = ft.Row([history_text], alignment = ft.MainAxisAlignment.END)
+    text_clear = ft.Row([button_clear], alignment=ft.MainAxisAlignment.START)
+
 
     page.add(name_input,
              greeting_text,
              theme_button,
              theme_text,
+             text_clear,
              story_greeting_text)
 
 ft.app(target=main)
